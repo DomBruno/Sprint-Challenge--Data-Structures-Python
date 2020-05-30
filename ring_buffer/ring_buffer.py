c5e0b1201661cce233de32712de8203a4923c7d0
Dominick Bruno <dbruno@unseen.is>
2020-05-29 22:26:56 -0400

    6/6 passing, Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,24 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        # Initialize capacity and empty buffer, yawn; had to add a location pointer
+        self.capacity = capacity
+        self.buffer_items = [None]*capacity
+        self.location = 0
+        
 
     def append(self, item):
-        pass
+        # Point new item to location
+        self.buffer_items[self.location] = item
 
+        # Check to see if capacity was reached, if so, assign it to position 0
+        if self.location >= len(self.buffer_items) - 1:
+            self.location = 0
+        
+        # else advance location
+        else:
+            self.location = self.location + 1
+
+            
     def get(self):
-        pass
+        # return contents of buffer if not none
+        return [item for item in self.buffer_items if item is not None]
